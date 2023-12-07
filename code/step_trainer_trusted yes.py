0b77ca3af5978bf7e2ea85bb6f6e176a889fdca1
oliviasherlynb <146457122+oliviasherlynb@users.noreply.github.com>
2023-12-07 09:40:47 +0000

    Update step_trainer_trusted yes.py

--- code/step_trainer_trusted yes.py
+++ code/step_trainer_trusted yes.py
@@ -4,6 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -12,64 +14,67 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node customer_curated
-customer_curated_node1701882239575 = glueContext.create_dynamic_frame.from_catalog(
+# Script generated for node step_trainer_landing
+AWSGlueDataCatalog_node1701941610891 = glueContext.create_dynamic_frame.from_catalog(
     database="stedi-database",
-    table_name="customer_curated",
-    transformation_ctx="customer_curated_node1701882239575",
+    table_name="step_trainer_landing",
+    transformation_ctx="AWSGlueDataCatalog_node1701941610891",
 )
 
-# Script generated for node Amazon S3
-AmazonS3_node1701884977791 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://os-stedi-lakehouse/step_trainer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="AmazonS3_node1701884977791",
+# Script generated for node customer_curated
+AWSGlueDataCatalog_node1701941620998 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi-database",
+    table_name="customer_curated",
+    transformation_ctx="AWSGlueDataCatalog_node1701941620998",
 )
 
 # Script generated for node Join
-Join_node1701882330408 = Join.apply(
-    frame1=customer_curated_node1701882239575,
-    frame2=AmazonS3_node1701884977791,
+Join_node1701941630465 = Join.apply(
+    frame1=AWSGlueDataCatalog_node1701941620998,
+    frame2=AWSGlueDataCatalog_node1701941610891,
     keys1=["serialnumber"],
-    keys2=["serialNumber"],
-    transformation_ctx="Join_node1701882330408",
+    keys2=["serialnumber"],
+    transformation_ctx="Join_node1701941630465",
+)
+
+# Script generated for node Drop Duplicates
+DropDuplicates_node1701941678060 = DynamicFrame.fromDF(
+    Join_node1701941630465.toDF().dropDuplicates(),
+    glueContext,
+    "DropDuplicates_node1701941678060",
 )
 
 # Script generated for node Drop Fields
-DropFields_node1701882350791 = DropFields.apply(
-    frame=Join_node1701882330408,
+DropFields_node1701941683948 = DropFields.apply(
+    frame=DropDuplicates_node1701941678060,
     paths=[
         "customername",
         "email",
         "phone",
         "birthday",
-        "serialnumber",
         "registrationdate",
         "lastupdatedate",
         "sharewithresearchasofdate",
         "sharewithpublicasofdate",
         "sharewithfriendsasofdate",
+        "`.serialnumber`",
     ],
-    transformation_ctx="DropFields_node1701882350791",
+    transformation_ctx="DropFields_node1701941683948",
 )
 
-# Script generated for node step_trainer_trusted
-step_trainer_trusted_node1701882385162 = glueContext.getSink(
+# Script generated for node Amazon S3
+AmazonS3_node1701941704318 = glueContext.getSink(
     path="s3://os-stedi-lakehouse/step_trainer/trusted/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="step_trainer_trusted_node1701882385162",
+    transformation_ctx="AmazonS3_node1701941704318",
 )
-step_trainer_trusted_node1701882385162.setCatalogInfo(
-    catalogDatabase="stedi-database", catalogTableName="step_train_trusted"
+AmazonS3_node1701941704318.setCatalogInfo(
+    catalogDatabase="stedi-database", catalogTableName="step_trainer_trusted"
 )
-step_trainer_trusted_node1701882385162.setFormat("json")
-step_trainer_trusted_node1701882385162.writeFrame(DropFields_node1701882350791)
+AmazonS3_node1701941704318.setFormat("json")
+AmazonS3_node1701941704318.writeFrame(DropFields_node1701941683948)
+
 job.commit()
