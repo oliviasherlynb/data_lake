352cd9f6ae211db19f977a027693ea6aac1e21d7
oliviasherlynb <146457122+oliviasherlynb@users.noreply.github.com>
2023-12-07 10:22:16 +0000

    Update machine_learning_curated.py

--- code/machine_learning_curated.py
+++ code/machine_learning_curated.py
@@ -15,54 +15,55 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node step_trainer_trusted
-step_trainer_trusted_node1701807919569 = glueContext.create_dynamic_frame.from_catalog(
+AWSGlueDataCatalog_node1701943779126 = glueContext.create_dynamic_frame.from_catalog(
     database="stedi-database",
     table_name="step_trainer_trusted",
-    transformation_ctx="step_trainer_trusted_node1701807919569",
+    transformation_ctx="AWSGlueDataCatalog_node1701943779126",
 )
 
 # Script generated for node accelerometer_trusted
-accelerometer_trusted_node1701807995037 = glueContext.create_dynamic_frame.from_catalog(
+AWSGlueDataCatalog_node1701943822552 = glueContext.create_dynamic_frame.from_catalog(
     database="stedi-database",
     table_name="accelerometer_trusted",
-    transformation_ctx="accelerometer_trusted_node1701807995037",
+    transformation_ctx="AWSGlueDataCatalog_node1701943822552",
 )
 
 # Script generated for node Join
-Join_node1701808012239 = Join.apply(
-    frame1=accelerometer_trusted_node1701807995037,
-    frame2=step_trainer_trusted_node1701807919569,
-    keys1=["timestamp"],
-    keys2=["sensorreadingtime"],
-    transformation_ctx="Join_node1701808012239",
+Join_node1701943919837 = Join.apply(
+    frame1=AWSGlueDataCatalog_node1701943779126,
+    frame2=AWSGlueDataCatalog_node1701943822552,
+    keys1=["sensorreadingtime"],
+    keys2=["timestamp"],
+    transformation_ctx="Join_node1701943919837",
 )
 
 # Script generated for node Drop Duplicates
-DropDuplicates_node1701809688990 = DynamicFrame.fromDF(
-    Join_node1701808012239.toDF().dropDuplicates(),
+DropDuplicates_node1701944217871 = DynamicFrame.fromDF(
+    Join_node1701943919837.toDF().dropDuplicates(),
     glueContext,
-    "DropDuplicates_node1701809688990",
+    "DropDuplicates_node1701944217871",
 )
 
 # Script generated for node Drop Fields
-DropFields_node1701808101873 = DropFields.apply(
-    frame=DropDuplicates_node1701809688990,
+DropFields_node1701944321996 = DropFields.apply(
+    frame=DropDuplicates_node1701944217871,
     paths=["user"],
-    transformation_ctx="DropFields_node1701808101873",
+    transformation_ctx="DropFields_node1701944321996",
 )
 
 # Script generated for node machine_learning_curated
-machine_learning_curated_node1701808110690 = glueContext.getSink(
+AmazonS3_node1701944330266 = glueContext.getSink(
     path="s3://os-stedi-lakehouse/final_data/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="machine_learning_curated_node1701808110690",
+    transformation_ctx="AmazonS3_node1701944330266",
 )
-machine_learning_curated_node1701808110690.setCatalogInfo(
+AmazonS3_node1701944330266.setCatalogInfo(
     catalogDatabase="stedi-database", catalogTableName="machine_learning_curated"
 )
-machine_learning_curated_node1701808110690.setFormat("json")
-machine_learning_curated_node1701808110690.writeFrame(DropFields_node1701808101873)
+AmazonS3_node1701944330266.setFormat("json")
+AmazonS3_node1701944330266.writeFrame(DropFields_node1701944321996)
+
 job.commit()
